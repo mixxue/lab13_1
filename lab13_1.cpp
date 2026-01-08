f07764eaab2c43d85b82b67a6c0fec67fa399704
mixxue <pison.mick1@gmail.com>
2026-01-08 09:48:02 +0700

    Update lab13_1.cpp

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iomanip>
 #include <cmath>
 using namespace std;
 
@@ -10,12 +9,48 @@ int main(){
     int N = sizeof(A)/sizeof(A[0]);
     double B[6];
     stat(A,N,B);
-    cout << fixed << setprecision(2);
-    cout << "Arithmetic Mean = " << B[0] << endl;
-    cout << "Standard Deviation = " << B[1] << endl;
-    cout << "Geometric Mean = " << B[2] << endl;
-    cout << "Harmonic Mean = " << B[3] << endl;
-    cout << "Max = " << B[4] << endl;
-    cout << "Min = " << B[5];
+    cout << "Arithmetic Mean = " << B[0];
+    cout << "\nStandard Deviation = " << B[1];
+    cout << "\nGeometric Mean = " << B[2];
+    cout << "\nHarmonic Mean = " << B[3];
+    cout << "\nMax = " << B[4];
+    cout << "\nMin = " << B[5];
     return 0;
+}
+
+void stat(const double A[],int N,double B[]){
+    double sum = 0;
+    for (int i = 0; i < N; i++){
+        sum += A[i];
+    }
+    B[0] = sum / N;
+
+    double sumSD = 0;
+    for (int i = 0; i < N; i++){
+       sumSD += pow(A[i] - B[0], 2);
+    }
+    B[1] = sqrt(sumSD / N);
+
+    double sumGD = 1;
+    for (int i = 0; i < N; i++){
+       sumGD *= A[i];
+    }
+    B[2] = pow(sumGD, 1.0 / N);
+    
+    double sumHM = 0;
+    for (int i = 0; i < N; i++){
+        sumHM += 1.0 / A[i];
+    }
+    B[3] = N / sumHM;
+
+    B[4] = A[0];
+    B[5] = A[0];
+    for(int i = 1; i < N; i++){
+   if (A[i] > B[4]){
+            B[4] = A[i]; 
+        }
+        if (A[i] < B[5]){
+            B[5] = A[i]; 
+        }
+    }
 }
